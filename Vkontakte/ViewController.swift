3ed21bce5b74aec9a2c172f17791ae3e868a3979
SimonSolv <4to5work@gmail.com>
2023-03-02 01:02:22 +0300

    Initial Commit

---
+++ Vkontakte/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vkontakte
+//
+//  Created by Simon Pegg on 02.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
